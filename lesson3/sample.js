30e967ebf123018abaa36088741655ce51b22da5
KouNakagawa <rarai0626@gmail.com>
2021-10-25 19:41:30 +0900

    [Finish] lesson3

---
+++ lesson3/sample.js
@@ -0,0 +1,63 @@
+// varによる再宣言
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// constでは再宣言と再代入ができない
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// var str = "webcamp" //グローバルスコープ
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello" //関数スコープ
+// }
+
+// foo()
+// console.log(y)
+
+// function foo() {
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+// foo()
+// letはブロックスコープの変数を定義する
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+// console.log(i)
+
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
