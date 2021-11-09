764daa31f4c2d5686ea2497ac3e0c82817c3af79
KouNakagawa <rarai0626@gmail.com>
2021-11-09 15:38:05 +0900

    [Finish] lesson6

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,6 +1,19 @@
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000ff'});
+// });
+
 $(function(){
-  $('.box').css({
-    'background-color': '#0000FF',
-    'height': '100px'
-  });
+  $('.box1').hide();
 });
